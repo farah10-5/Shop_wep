873a602579ae751f54e7beeac10315a7387ffca6
farah510 <168647092+farah510@users.noreply.github.com>
2024-07-20 11:23:46 +0200

    IntegrationTesting

---
+++ ShopTest/IntegrationTests/FlowerServiceTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ShopDomain.Entities;
+using ShopPersistance;
+using ShopServices;
+
+namespace Shop.Test.IntegrationTests
+{
+    public class FlowerServiceTests
+    {
+        private DbContextOptions<ShopContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+        private IDbContextFactory<ShopContext> GetDbContextFactoryAsync(DbContextOptions<ShopContext> options)
+        {
+            var mockDbFactory = new Mock<IDbContextFactory<ShopContext>>();
+            mockDbFactory.Setup(f => f.CreateDbContext()).Returns(() => new ShopContext(options));
+            return mockDbFactory.Object;
+
+        }
+        [Fact]
+        public async Task Save_ShouldAddFlower()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            var flower = new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر ", FlowertPrice = "35" };
+
+            // Act
+            await service.Save(flower);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var savedFlower = await context.Flowers.FirstOrDefaultAsync(a => a.FlowerName == "Flower1");
+            Assert.NotNull(savedFlower);
+        }
+        [Fact]
+        public async Task Get_ShouldReturnFlowerByFlowerId()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            var flower = new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" };
+            await service.Save(flower);
+
+            // Act
+            var fetchedFlower = await service.Get(flower.FlowerId);
+
+            // Assert
+            Assert.NotNull(fetchedFlower);
+            Assert.Equal(flower.FlowerName, fetchedFlower.FlowerName);
+        }
+        [Fact]
+        public async Task GetList_ShouldReturnFlowersByFlowerName()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            await service.Save(new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" });
+            await service.Save(new Flower { FlowerName = "Flower2", FlowerDescription = "لون احمر", FlowertPrice = "35" });
+
+            // Act
+            var flowers = await service.GetList("Flower");
+
+            // Assert
+            Assert.Equal(2, flowers.Count);
+        }
+        [Fact]
+        public async Task GetAll_ShouldReturnAllFlowers()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            await service.Save(new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" });
+            await service.Save(new Flower { FlowerName = "Flower2", FlowerDescription = "لون احمر", FlowertPrice = "35" });
+
+            // Act
+            var flowers = await service.GetAll();
+            // Assert
+            Assert.Equal(2, flowers.Count);
+        }
+        [Fact]
+        public async Task Delete_ShouldRemoveFlower()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            var flower = new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" };
+            await service.Save(flower);
+
+            // Act
+            await service.Delete(flower);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var deletedFlower = await context.Flowers.FindAsync(flower.FlowerId);
+            Assert.Null(deletedFlower);
+        }
+        [Fact]
+        public async Task Update_ShouldModifyFlower()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new FlowerServices(factory);
+            var flower = new Flower { FlowerName = "Flower1", FlowertPrice = "35", FlowerDescription = " لون احمر" };
+            await service.Save(flower);
+
+            // Act
+            flower.FlowerName = "Updated Flower";
+            flower.FlowertPrice = "36";
+            flower.FlowerDescription = "زهرة عطرية";
+            await service.Update(flower);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var updatedFlower = await context.Flowers.FindAsync(flower.FlowerId);
+            Assert.Equal("Updated Flower", updatedFlower.FlowerName);
+            Assert.Equal("36", updatedFlower.FlowertPrice);
+            Assert.Equal("زهرة عطرية", updatedFlower.FlowerDescription);
+        }
+
+
+    }
+}
---
+++ ShopTest/IntegrationTests/GiftServiceTests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using ShopDomain.Entities;
+using ShopPersistance;
+using ShopServices;
+
+namespace Shop.Test.IntegrationTests
+{
+    public class GiftServiceTests
+    {
+        private DbContextOptions<ShopContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+        private IDbContextFactory<ShopContext> GetDbContextFactory(DbContextOptions<ShopContext> options)
+        {
+            var mockFactory = new Mock<IDbContextFactory<ShopContext>>();
+            mockFactory.Setup(f => f.CreateDbContext()).Returns(() => new ShopContext(options));
+            return mockFactory.Object;
+        }
+        [Fact]
+        public async Task Save_ShouldAddGift()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+
+            // Act
+            await service.Save(gift);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var savedGift = await context.Gifts.FirstOrDefaultAsync(b => b.GiftName == "Gift1");
+            Assert.NotNull(savedGift);
+        }
+        [Fact]
+        public async Task GetById_ShouldReturnGiftByGiftId()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+            await service.Save(gift);
+
+            // Act
+            var fetchedGift = await service.Get(gift.GiftId);
+
+            // Assert
+            Assert.NotNull(fetchedGift);
+            Assert.Equal(gift.GiftName, fetchedGift.GiftName);
+        }
+       
+        [Fact]
+        public async Task GetList_ShouldReturnGiftsByGiftName()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            await service.Save(new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" });
+            await service.Save(new Gift { GiftName = "Gift2", GiftType = "بوكس مفتوح", GiftDescription = "لون ابيض", GiftPrice = "100" });
+
+            // Act
+            var gifts = await service.GetList("Gift");
+
+            // Assert
+            Assert.Equal(2, gifts.Count);
+        }
+        [Fact]
+        public async Task GetAll_ShouldReturnAllGifts()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            await service.Save(new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" });
+            await service.Save(new Gift { GiftName = "Gift2", GiftType = "بوكس مفتوح", GiftDescription = "لون ابيض", GiftPrice = "100" });
+
+            // Act
+            var gifts = await service.GetAll();
+
+            // Assert
+            Assert.Equal(2, gifts.Count);
+        }
+        [Fact]
+        public async Task Delete_ShouldRemoveGift()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices (factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+            await service.Save(gift);
+
+            // Act
+            await service.Delete(gift);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var deletedGift = await context.Gifts.FindAsync(gift.GiftId);
+            Assert.Null(deletedGift);
+        }
+        [Fact]
+        public async Task Update_ShouldModifyGift()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+            await service.Save(gift);
+
+            // Act
+            gift.GiftName = "Updated Gift";
+            gift.GiftType = "بوكس صغير";
+            await service.Update(gift);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var updatedGift = await context.Gifts.FindAsync(gift.GiftId);
+            Assert.Equal("Updated Gift", updatedGift.GiftName);
+            Assert.Equal("بوكس صغير", updatedGift.GiftType);
+        }
+        [Fact]
+        public async Task AddFlowerToGift_ShouldAddFlower()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+            var flower = new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" };
+            await service.Save(gift);
+
+            // Act
+            await service.AddFlowerToGift(gift, flower);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var savedGift = await context.Gifts.Include(b => b.Flowers).FirstOrDefaultAsync(b => b.GiftId == gift.GiftId);
+            Assert.NotNull(savedGift);
+            Assert.Contains(savedGift.Flowers, a => a.FlowerName == "Flower1");
+        }
+        [Fact]
+        public async Task RemoveFlowerFromGift_ShouldRemoveFlower()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactory(options);
+            var service = new GiftServices(factory);
+            var gift = new Gift { GiftName = "Gift1", GiftType = "بوكس مغلق", GiftDescription = "لون وردي", GiftPrice = "80" };
+            var flower = new Flower { FlowerName = "Flower1", FlowerDescription = "لون احمر", FlowertPrice = "35" };
+            await service.Save(gift);
+            await service.AddFlowerToGift(gift, flower);
+
+            // Act
+            await service.RemoveFlowerFromGift(gift, flower);
+
+            // Assert
+            using var context = new ShopContext(options);
+            var savedGift = await context.Gifts.Include(b => b.Flowers).FirstOrDefaultAsync(b => b.GiftId == gift.GiftId);
+            Assert.NotNull(savedGift);
+            Assert.DoesNotContain(savedGift.Flowers, a => a.FlowerName == "Flower1");
+        }
+    }
+
+}
